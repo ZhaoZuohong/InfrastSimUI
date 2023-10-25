2d51dd22b1137613ee664476383e35069cb03248
Zhao Zuohong <zhbaor@zhaozuohong.vip>
2023-10-25 12:03:06 +0800

    wasm移除unplugin-auto-import依赖以避免与dotnet.js冲突

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 
@@ -10,14 +9,6 @@ import { NaiveUiResolver } from 'unplugin-vue-components/resolvers'
 export default defineConfig({
   plugins: [
     vue(),
-    AutoImport({
-      imports: [
-        'vue',
-        {
-          'naive-ui': ['useDialog', 'useMessage', 'useNotification', 'useLoadingBar']
-        }
-      ]
-    }),
     Components({
       resolvers: [NaiveUiResolver()]
     })
