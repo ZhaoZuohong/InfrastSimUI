a2c5f29032d893283d48ca95ccfae1790742e31f
Funny_ppt <1763341376@qq.com>
2023-11-06 13:35:37 +0800

    更新wasm小工具，更新.gitignore

---
+++ update.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# GitHub 仓库和工作流信息
+REPO_OWNER="Funny-ppt"
+REPO_NAME="InfrastSim"
+WORKFLOW_NAME="dotnet.yml"
+ARTIFACT_NAME="AppBundle"
+
+# 获取最新工作流的运行号
+LATEST_RUN=$(curl -s "https://api.github.com/repos/$REPO_OWNER/$REPO_NAME/actions/workflows/$WORKFLOW_NAME/runs" | jq -r '.workflow_runs[0].id')
+
+# 检查是否需要下载 Artifact
+RUNID_FILE="tmp/last_run_id"
+if [ -f "$RUNID_FILE" ]; then
+  LAST_RUNID=$(cat "$RUNID_FILE")
+else
+  LAST_RUNID=""
+fi
+
+if [ "$LAST_RUNID" != "$LATEST_RUN" ]; then
+  # 下载 Artifact
+  echo "Downloading $ARTIFACT_NAME..."
+  if [ ! -d "tmp" ]; then
+    mkdir tmp
+  else
+    rm -r tmp/*
+    rm tmp/.stamp
+  fi
+  gh run download "$LATEST_RUN" -R "$REPO_OWNER/$REPO_NAME" -n "$ARTIFACT_NAME" -D tmp
+  rm -r public/assets/_framework
+  rm -r src/wasm
+  cp -r tmp/_framework public/assets/_framework
+  cp -r tmp/_framework src/wasm
+
+  # 保存最新的运行号到文件中
+  echo "$LATEST_RUN" > "$RUNID_FILE"
+  echo "Downloaded and updated to the latest run ID: $LATEST_RUN"
+else
+  echo "Artifact is already up to date with the latest run ID: $LATEST_RUN"
+fi
